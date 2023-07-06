ac7c28743651eeeeda25355e21ca5e6c34afd6d4
yenare-nikita <nikitayenare4@gmail.com>
2023-07-06 07:56:40 +0530

    angular project

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: '<h1>Hello world</h1>'
+  
+})
+export class AppComponent {
+  title = 'angulardemo1';
+}
